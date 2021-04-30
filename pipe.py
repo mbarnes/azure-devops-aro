905b4d6b2f701fe6e14c4789e1ba269042d3ff2e
Matthew Barnes <mbarnes@fedoraproject.org>
2021-04-30 14:31:04 -0400

    Repurpose pipe.py to download build logs for a pipeline run
    Given a pipeline name and optionally a run name within that
    pipeline, download and extract all corresponding build logs
    to a folder per run:
    
      ./<pipeline-name>/<run-name>/

--- pipe.py
+++ pipe.py
@@ -1,49 +1,89 @@
 #!/usr/bin/python3
 
 import argparse
+import errno
+import io
 import os
-
-from datetime import datetime, timedelta
+import sys
+import zipfile
 
 from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
-import pytz
 
 PAT_ENVIRON_VARNAME = 'AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN'
 DEFAULT_ORGANIZATION_URL = 'https://msazure.visualstudio.com'
 DEFAULT_PROJECT = 'AzureRedHatOpenShift'
+DEFAULT_PIPELINE = 'PROD - Deploy all RPs'
+
+def safe_mkdir(path, mode=0o777):
+    try:
+        os.makedirs(path, mode)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 
 def main(args):
-    # Set historical time frame in days
-    days_to_subtract = 1
-    d = datetime.utcnow().replace(tzinfo=pytz.UTC) - timedelta(days=days_to_subtract)
-
     # Create a connection to the org
     credentials = BasicAuthentication('', args.personal_access_token)
     connection = Connection(base_url=args.organization_url, creds=credentials)
 
-    # Get a client
-    core_client = connection.clients_v6_0.get_build_client()
+    # Get needed clients
+    client_factory = connection.clients_v6_0
+    pipelines_client = client_factory.get_pipelines_client()
+    build_client = client_factory.get_build_client()
 
-    # Get builds for pipeline
-    get_builds_response = core_client.get_builds(args.project)
-    if get_builds_response:
-        # Note: Unfinished builds have a finish_time of None
-        recent_builds = [b for b in get_builds_response if b.finish_time and d < b.finish_time]
-    else:
-        recent_builds = []
+    # Get pipelines
+    pipelines_by_name = {}
+    pipelines = pipelines_client.list_pipelines(args.project)
+    if pipelines:
+        pipelines_by_name = {p.name: p for p in pipelines}
+    try:
+        pipeline = pipelines_by_name[args.pipeline_name]
+    except KeyError as ex:
+        print('Invalid pipeline name "{}"'.format(ex.args[0]), file=sys.stderr)
+        print('Valid pipeline names:', file=sys.stderr)
+        for name in sorted(pipelines_by_name):
+            print('  ' + name, file=sys.stderr)
+        sys.exit(2)
 
-    for build in recent_builds:
-        # Get logs info from build
-        get_build_logs_response = core_client.get_build_logs(args.project, build.id)
-        if get_build_logs_response:
-            for log in get_build_logs_response:
-                # Get logs lines from logs from log info
-                get_build_log_lines_response = core_client.get_build_log_lines(args.project, build.id, log.id)
-                if get_build_log_lines_response:
-                    for log_lines in get_build_log_lines_response:
-                        # Outputs logs within given historical timeframe for project
-                        print(log_lines)
+    # Get the corresponding build definition
+    build_definition = build_client.get_definitions(
+        args.project,
+        name=pipeline.name,
+        path=pipeline.folder).pop()
+
+    # Get runs for pipeline
+    runs_by_name = {}
+    runs = pipelines_client.list_runs(args.project, pipeline.id)
+    if runs:
+        runs_by_name = {r.name: r for r in runs}
+    if args.run_name:
+        try:
+            runs_by_name = {args.run_name: runs_by_name[args.run_name]}
+        except KeyError as ex:
+            print('Invalid run name "{}"'.format(ex.args[0]), file=sys.stderr)
+            print('Valid run names:', file=sys.stderr)
+            for name in sorted(runs_by_name):
+                print('  ' + name, file=sys.stderr)
+            sys.exit(2)
+
+    # Get the corresponding build for each run
+    for run_name in runs_by_name:
+        build = build_client.get_builds(
+            args.project,
+            definitions=(build_definition.id,),
+            build_number=run_name).pop()
+
+        # Download and save logs from the build
+        run_logs_dir = os.path.join(os.getcwd(), pipeline.name, run_name)
+        safe_mkdir(run_logs_dir)
+        byte_stream = io.BytesIO()
+        for data in build_client.get_build_logs_zip(args.project, build.id):
+            byte_stream.write(data)
+        with zipfile.ZipFile(byte_stream) as zipped_logs:
+            zipped_logs.extractall(run_logs_dir)
 
 
 if __name__ == '__main__':
@@ -61,5 +101,12 @@ if __name__ == '__main__':
         '--project',
         default=DEFAULT_PROJECT,
         help='project name (default={})'.format(DEFAULT_PROJECT))
+    parser.add_argument(
+        '--pipeline', metavar='NAME', dest='pipeline_name',
+        default=DEFAULT_PIPELINE,
+        help='pipeline name (default={})'.format(DEFAULT_PIPELINE))
+    parser.add_argument(
+        '--run', metavar='NAME', dest='run_name',
+        help='run name for pipeline')
     args = parser.parse_args()
     main(args)
